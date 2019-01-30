17d9eb23d7515bdc0d6bc25e6e2c2f74e5afb906
amazing-username <kundeng94@gmail.com>
2019-01-30 07:44:01 -0500

    Created Status model #1

--- api/models.py
+++ api/models.py
@@ -3,3 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class Status(models.Model):
+    message = models.CharField(max_length=50)
+    state = models.CharField(max_length=30)
+    date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
